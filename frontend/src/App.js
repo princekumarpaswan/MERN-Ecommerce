0b22738cb5818526d676f50e4c9ff02b2c318c86
princekumarpaswan <prasantandal123@gmail.com>
2023-06-08 21:45:57 +0530

    Completed: NavBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Header from './component/layout/Header.js';
 
 function App() {
   return (
-    <></>
+    <>
+      <Header />
+    </>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,5 +8,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
 
