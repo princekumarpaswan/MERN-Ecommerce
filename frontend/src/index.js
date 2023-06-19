8594fa36c2c95955f5189582decc32e63d9f3c2e
princekumarpaswan <prasantandal123@gmail.com>
2023-06-19 23:24:02 +0530

    final commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,12 @@ import store from "./store"
 import App from './App';
 import { positions, transitions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from 'react-alert-template-basic';
-import axios from 'axios';
+// import axios from 'axios';
 
 
 // axios.defaults.baseURL = process.env.REACT_APP_BACKEND_API;
 // console.log(process.env.REACT_APP_BACKEND_API);
-axios.defaults.baseURL = "https://princee-commerce.onrender.com"
+// axios.defaults.baseURL = "https://princee-commerce.onrender.com"
 
 const options = {
   timeout: 5000,
