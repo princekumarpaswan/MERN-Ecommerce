adcf9b0997e1fd7e4f865777fb396fc739573d05
princekumarpaswan <prasantandal123@gmail.com>
2023-06-19 23:04:37 +0530

    added a cookie domain

--- backend/util/jwtTokens.js
+++ backend/util/jwtTokens.js
@@ -7,7 +7,8 @@ const sendToken = (user, statusCode, res) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true
+        httpOnly: true,
+        domain: "http://localhost:3000"
     }
 
 
