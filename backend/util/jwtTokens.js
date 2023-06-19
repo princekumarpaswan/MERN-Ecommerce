589c9fa6b46641f7c576c87f38f45743515c781c
princekumarpaswan <prasantandal123@gmail.com>
2023-06-19 23:08:47 +0530

    Removed backend

--- backend/util/jwtTokens.js
+++ backend/util/jwtTokens.js
@@ -8,7 +8,6 @@ const sendToken = (user, statusCode, res) => {
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
-        domain: "http://localhost:3000"
     }
 
 
