f760e30745b7ad18ed51d7cf91091ba33025f65b
Prince Kumar paswan <43513990+princekumarpaswan@users.noreply.github.com>
2023-06-23 15:34:53 +0530

    Update jwtTokens.js

--- backend/util/jwtTokens.js
+++ backend/util/jwtTokens.js
@@ -2,7 +2,7 @@ const sendToken = (user, statusCode, res) => {
 
     const token = user.getJWTToken();
 
-    // options fro cokie
+    // options for cookie
     const options = {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
